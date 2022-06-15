ad8806f587c8380e5a8fbcf44b7a7068f3892f5c
VitaliiZaichkin <vitalii.zaichkin@introduct.tech>
2022-06-15 10:11:27 +0300

    Turns on email sending

--- src/routers/user.js
+++ src/routers/user.js
@@ -11,7 +11,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        // sendWelcomeEmail(user.email, user.name);
+        sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken();
         res.status(201).send({ user, token });
     } catch (error) {
@@ -105,7 +105,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove();
-        // sendCancellationEmail(req.user.email, req.user.name);
+        sendCancellationEmail(req.user.email, req.user.name);
         res.send(req.user);
     } catch (error) {
         res.status(500).send();
